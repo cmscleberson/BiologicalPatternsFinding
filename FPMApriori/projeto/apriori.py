5c62588420b0b2987664d25c3268da7659da73ca
Cleberson Marcelo dos Santos <33093380+cmscleberson@users.noreply.github.com>
2021-06-15 13:41:06 -0300

    Add files via upload

---
+++ FPMApriori/projeto/apriori.py
@@ -0,0 +1,77 @@
+from csv import reader
+from collections import defaultdict
+from itertools import chain, combinations
+from optparse import OptionParser
+from apriori_python.utils import *
+
+def apriori(itemSetList, minSup, minConf):
+    C1ItemSet = getItemSetFromList(itemSetList)
+    # Final result global frequent itemset
+    globalFreqItemSet = dict()
+    # Storing global itemset with support count
+    globalItemSetWithSup = defaultdict(int)
+
+    L1ItemSet = getAboveMinSup(
+        C1ItemSet, itemSetList, minSup, globalItemSetWithSup)
+    currentLSet = L1ItemSet
+    k = 2
+
+    # Calculating frequent item set
+    while(currentLSet):
+        # Storing frequent itemset
+        globalFreqItemSet[k-1] = currentLSet
+        # Self-joining Lk
+        candidateSet = getUnion(currentLSet, k)
+        # Perform subset testing and remove pruned supersets
+        candidateSet = pruning(candidateSet, currentLSet, k-1)
+        # Scanning itemSet for counting support
+        currentLSet = getAboveMinSup(
+            candidateSet, itemSetList, minSup, globalItemSetWithSup)
+        k += 1
+
+    rules = associationRule(globalFreqItemSet, globalItemSetWithSup, minConf)
+    rules.sort(key=lambda x: x[2])
+
+    return globalFreqItemSet, rules
+
+def aprioriFromFile(fname, minSup, minConf):
+    C1ItemSet, itemSetList = getFromFile(fname)
+
+    # Final result global frequent itemset
+    globalFreqItemSet = dict()
+    # Storing global itemset with support count
+    globalItemSetWithSup = defaultdict(int)
+
+    L1ItemSet = getAboveMinSup(
+        C1ItemSet, itemSetList, minSup, globalItemSetWithSup)
+    currentLSet = L1ItemSet
+    k = 2
+
+    # Calculating frequent item set
+    while(currentLSet):
+        # Storing frequent itemset
+        globalFreqItemSet[k-1] = currentLSet
+        # Self-joining Lk
+        candidateSet = getUnion(currentLSet, k)
+        # Perform subset testing and remove pruned supersets
+        candidateSet = pruning(candidateSet, currentLSet, k-1)
+        # Scanning itemSet for counting support
+        currentLSet = getAboveMinSup(
+            candidateSet, itemSetList, minSup, globalItemSetWithSup)
+        k += 1
+
+    rules = associationRule(globalFreqItemSet, globalItemSetWithSup, minConf)
+    rules.sort(key=lambda x: x[2])
+
+    return globalFreqItemSet, rules
+
+if __name__ == "__main__":
+
+    inputFile = "D:\\- Projetos Python\\apriori001\\projeto\\dataset\\dnaseq04.csv"
+    minSup = 0.5
+    minConf = 0.5
+
+    freqItemSet, rules = aprioriFromFile(inputFile,minSup, minConf)
+
+    for i in range(len(rules)):
+        print(rules[i])
